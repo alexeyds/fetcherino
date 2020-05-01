0e4e8a71a9cdc6dfa2c5e36d891e581d919c4eff
Alexey D <ord.alwo@gmail.com>
2020-05-02 01:13:37 +0300

    Content type helpers

---
+++ test/lib/content_type/helpers_test.js
@@ -0,0 +1,65 @@
+import test from "enhanced-tape";
+import { Response } from "node-fetch";
+import { isTextType, isJSONType, isFormDataType } from "content_type/helpers";
+
+function contentWithType(type) {
+  let headers;
+
+  if (type === null) {
+    headers = {};
+  } else {
+    headers = {"Content-Type": type};
+  }
+
+  return new Response(null, {headers});
+}
+
+test("content_type/helpers", function(t) {
+  t.test("isTextType()", function(t) {
+    t.test("false if content-type is null", function(t) {
+      t.false(isTextType(contentWithType(null)));
+  
+      t.end();
+    });
+
+    t.test("true if content-type is plain text", function(t) {
+      t.true(isTextType(contentWithType("text/plain;utf-8")));
+    
+      t.end();
+    });
+
+    t.test("false if content-type is not text", function(t) {
+      t.false(isTextType(contentWithType("text/html")));
+  
+      t.end();
+    });
+  });
+
+  t.test("isJSONType()", function(t) {
+    t.test("false if content-type is not JSON", function(t) {
+      t.false(isJSONType(contentWithType("text/html")));
+  
+      t.end();
+    });
+
+    t.test("true if content-type is JSON", function(t) {
+      t.true(isJSONType(contentWithType("application/json")));
+    
+      t.end();
+    });
+  });
+
+  t.test("isFormDataType()", function(t) {
+    t.test("false if content-type is not form-data", function(t) {
+      t.false(isFormDataType(contentWithType("text/html")));
+  
+      t.end();
+    });
+
+    t.test("true if content-type is form-data", function(t) {
+      t.true(isFormDataType(contentWithType("multipart/form-data; boundary=123")));
+    
+      t.end();
+    });
+  });
+});
