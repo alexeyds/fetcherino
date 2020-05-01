33acb0c253ca38e98d7c12a254d4c4f8a9387905
Alexey D <ord.alwo@gmail.com>
2020-05-02 00:49:52 +0300

    Better way of detecting Headers/FormData instances

--- test/lib/utils/object_test.js
+++ test/lib/utils/object_test.js
@@ -1,26 +1,7 @@
 import test from "enhanced-tape";
-import { hasMethod, isObject } from "utils/object";
+import { isObject } from "utils/object";
 
 test("utils/object", function(t) {
-  t.test("hasMethod()", function(t) {
-    t.test("false if object has no method", function(t) {
-      t.false(hasMethod({}, "test"));
-  
-      t.end();
-    });
-
-    t.test("true if object has method", function(t) {
-      t.true(hasMethod({test: () => {}}, "test"));
-    
-      t.end();
-    });
-
-    t.test("false if object's property is not a function", function(t) {
-      t.false(hasMethod({test: 1}, "test"));
-    
-      t.end();
-    });
-  });
 
   t.test("isObject()", function(t) {
     t.test("true if value is object", function(t) {
