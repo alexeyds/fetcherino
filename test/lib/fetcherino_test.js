cd61d984cf9aa38d54d298e001fc12fdcac2ed30
Alexey D <ord.alwo@gmail.com>
2020-05-09 16:50:51 +0300

    fetch.mock() now uses ExpectedRequest

--- test/lib/fetcherino_test.js
+++ test/lib/fetcherino_test.js
@@ -1,14 +1,104 @@
 import test from "enhanced-tape";
-import { mockFetch } from "fetcherino";
+import { ContentTypes } from "headers/content_type";
+import { buildFetch } from "fetcherino";
 
-test("mockFetch", function(t) {
-  t.test("throws on unmocked fetch attempt", function(t) {
-    let fetch = mockFetch();
+test("buildFetch", function(t) {
+  t.test("fetch()", function(t) {
+    t.test("throws on unmocked fetch attempt", function(t) {
+      let fetch = buildFetch();
 
-    t.throws(() => {
-      fetch("test.com");
-    }, /mock/);
+      t.throws(() => {
+        fetch("/test");
+      }, /mock/);
 
-    t.end();
+      t.end();
+    });
+
+    t.test("delegates request options", function(t) {
+      let fetch = buildFetch();
+      fetch.mock("/test", { requestMethod: "POST" });
+
+      t.doesNotThrow(() => {
+        fetch("/test", {method: "POST"});
+      });
+    
+      t.end();
+    });
+  });
+
+  t.test("fetch.mock()", function(t) {
+    t.test("mocks empty response", async function(t) {
+      let fetch = buildFetch();
+      fetch.mock("/test");
+
+      let r = await fetch("/test").then(r => r);
+
+      t.equal(r.status, 200);
+      t.equal(r.url, "/test");
+      t.equal(await r.text(), "");
+    
+      t.end();
+    });
+
+    t.test("matches expected requests", async function(t) {
+      let fetch = buildFetch();
+      fetch.mock("/test");
+
+      t.throws(() => {
+        fetch("/tests");
+      }, /mock/);
+    
+      t.end();
+    });
+
+    t.test("supports response options", async function(t) {
+      let fetch = buildFetch();
+      fetch.mock("/test", { responseBody: "test", responseStatus: 400, responseHeaders: {"Content-Type": "text"} });
+
+      let r = await fetch("/test");
+
+      t.equal(await r.text(), "test");
+      t.equal(r.status, 400);
+      t.equal(r.headers.get("Content-Type"), "text");
+    
+      t.end();
+    });
+
+    t.test("supports {requestMethod} options", async function(t) {
+      let fetch = buildFetch();
+      fetch.mock("/test", { requestMethod: "POST" });
+
+      t.throws(() => {
+        fetch("/test");
+      }, /mock/);
+    
+      t.end();
+    });
+
+    t.test("supports {requestBody} options", async function(t) {
+      let fetch = buildFetch();
+      fetch.mock("/test", { requestMethod: "POST", requestBody: "test" });
+
+      t.throws(() => {
+        fetch("/test", {method: "POST"});
+      }, /mock/);
+    
+      t.end();
+    });
+
+    t.test("supports {requestHeaders} options", async function(t) {
+      let fetch = buildFetch();
+      fetch.mock("/test", { 
+        requestMethod: "POST", 
+        requestBody: JSON.stringify({a: 1, b: 2}), 
+        requestHeaders: {"Content-Type": ContentTypes.JSON} 
+      });
+
+      t.doesNotThrow(() => {
+        fetch("/test", {method: "POST", body: JSON.stringify({b: 2, a: 1}), headers: {"Content-Type": ContentTypes.JSON}});
+      });
+    
+      t.end();
+    });
   });
 });
