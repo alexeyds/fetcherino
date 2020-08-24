ce1c35461a3048b624d45826e2f2fcca6bb011a3
Alexey D <ord.alwo@gmail.com>
2020-08-24 12:10:09 +0300

    Replace CachedBody with extractBody

--- test/lib/server/extract_body_test.js
+++ test/lib/server/extract_body_test.js
@@ -2,41 +2,32 @@ import test from "enhanced-tape";
 import ContentTypes from "content_type/types";
 import FormData from "fetch_api/form_data";
 import Request from "fetch_api/request";
-import CachedBody from "server/cached_body";
+import extractBody from "server/extract_body";
 
 test("CachedBody", function(t) {
-  function buildBody({body, contentType}) {
+  function buildRequest({body, contentType}) {
     let headers = contentType ? {"content-type": contentType} : undefined;
-    let request = new Request("/", {body, headers, method: "POST"});
-    return new CachedBody(request);
+    return new Request("/", {body, headers, method: "POST"});
   }
 
   t.test("#extract", function(t) {
     t.test("extracts request body", async function(t) {
-      let body = buildBody({body: "foobar"});
-      let result = await body.extract();
-
-      t.equal(result, "foobar");
-    });
-
-    t.test("caches request body", async function(t) {
-      let body = buildBody({body: "foobar"});
-      await body.extract();
-      let result = await body.extract();
+      let request = buildRequest({body: "foobar"});
+      let result = await extractBody(request);
 
       t.equal(result, "foobar");
     });
 
     t.test("can extract bodies with no content-type", async function(t) {
-      let body = buildBody({});
-      let result = await body.extract();
+      let request = buildRequest({});
+      let result = await extractBody(request);
 
       t.equal(result, "");
     });
 
     t.test("parses JSON bodies", async function(t) {
-      let body = buildBody({body: JSON.stringify({foo: "bar"}), contentType: ContentTypes.json()});
-      let result = await body.extract();
+      let request = buildRequest({body: JSON.stringify({foo: "bar"}), contentType: ContentTypes.json()});
+      let result = await extractBody(request);
 
       t.same(result, {foo: "bar"});
     });
@@ -44,16 +35,16 @@ test("CachedBody", function(t) {
     t.test("parses formData", async function(t) {
       let fd = new FormData();
       fd.append("foo", "bar");
-      let body = buildBody({body: fd});
-      let result = await body.extract();
+      let request = buildRequest({body: fd});
+      let result = await extractBody(request);
 
       t.same(result, {foo: "bar"});
     });
 
     t.test("parses form-url-encoded as formdata", async function(t) {
       let form = new URLSearchParams("foo[]=bar&foo[]=baz");
-      let body = buildBody({body: form});
-      let result = await body.extract();
+      let request = buildRequest({body: form});
+      let result = await extractBody(request);
 
       t.same(result, {foo: ["bar", "baz"]});
     });
