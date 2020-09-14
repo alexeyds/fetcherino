3d11c16a1013c096b459642d286567ef420d9faa
Alexey D <ord.alwo@gmail.com>
2020-09-14 17:43:28 +0300

    README layout

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # fetcherino
 A light-weight mocking tool for replacing fetch in your tests
 
+## Installation
+```cmd
+npm i fetcherino --save-dev
+```
+
+## Example Usage
+```js
+import { buildFetch } from "fetch";
+let fetch = buildFetch();
+
+fetch.mock('/test');
+fetch("/test").then(console.log) // => Response { status: 200, body: '' }
+// fetch.mock() only mocks response once
+fetch("/test").catch(console.log) // => Unexpected fetch: GET /test
+```
+
+
 ## License
 MIT
--- test/dist/readme_test.js
+++ test/dist/readme_test.js
@@ -1,6 +1,24 @@
 import test from "enhanced-tape";
+import { testRejects } from "test/support/promise_helpers";
+import { buildFetch } from "dist/fetcherino";
+
+// mock once
+// partial body matching
+// mock reset
+// formdata support
+// fetcherino/matchers
 
 test("README test", function(t) {
-  t.test("fetch", function(_t) {
+  t.setup(() => ({ fetch: buildFetch() }));
+
+  t.test("example usage", function(t) {
+    t.test("simple example", async function(t, {fetch}) {
+      fetch.mock('/test');
+      let response = await fetch('/test');
+
+      t.equal(response.status, 200);
+      t.equal(await response.text(), '');
+      await testRejects(t, fetch("/test"), /Unexpected/);
+    });
   });
 });
